ed265660c733d8b24c8fbd5faca8883cc6c7fd76
Carlos Ferreira <63306370+CarlosSTS@users.noreply.github.com>
2026-05-20 09:40:51 -0300

    feat: Add more information for android and ios

--- src/providers/ios.js
+++ src/providers/ios.js
@@ -2,25 +2,29 @@ export const getIosVersion = async (bundleId, country) => {
   // Adds a random number to the end of the URL to prevent caching
   const url = `https://itunes.apple.com/lookup?bundleId=${bundleId}&country=${country}&_=${new Date().valueOf()}`;
 
-  let res = await fetch(url);
+  const response = await fetch(url);
 
-  const data = await res.json();
+  const data = await response.json();
 
-  if (!data || !("results" in data)) {
+  if (!data?.results) {
     throw new Error("Unknown error connecting to iTunes.");
   }
   if (!data.results.length) {
     throw new Error("App for this bundle ID not found.");
   }
 
-  res = data.results[0];
-
+  const res = data.results[0];
   return {
-    version: res.version || null,
-    released: res.currentVersionReleaseDate || res.releaseDate || null,
+    version: res.version || "",
+    releasedAt: res.currentVersionReleaseDate || res.releaseDate || "",
+    updateAt: res.currentVersionReleaseDate || "",
     notes: res.releaseNotes || "",
-    url: res.trackViewUrl || res.artistViewUrl || res.sellerUrl || null,
-    country,
-    lastChecked: (new Date()).toISOString()
+    url: res.trackViewUrl || res.artistViewUrl || res.sellerUrl || "",
+    country: country || "",
+    bundleId: bundleId || "",
+    lastChecked: new Date().toISOString(),
+    appIcon: res.artworkUrl512 || res.artworkUrl100 || "",
+    appName: res.trackName || "",
+    description: res.description || "",
   };
 };
